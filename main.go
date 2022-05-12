e731c9cb304cec5bd15a057f3794bb2dee48a550
chaos-yeats <chaos.ye@outlook.com>
2022-05-13 00:32:17 +0800

    添加上下文

--- main.go
+++ main.go
@@ -11,18 +11,15 @@ func main() {
 	fmt.Println("start http server...")
 	r := yee.New()
 
-	r.Get("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "URL.Path=%q\n", req.URL.Path)
+	r.Get("/", func(c *yee.Context) {
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 	})
 
-	r.Get("/hello", func(w http.ResponseWriter, req *http.Request) {
-		for k, v := range req.Header {
-			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-		}
-	})
-
-	r.Post("/world", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "URL.Method=%q\n", req.Method)
+	r.Post("/login", func(c *yee.Context) {
+		c.JSON(http.StatusOK, yee.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
 	})
 
 	r.Run(":9999")
