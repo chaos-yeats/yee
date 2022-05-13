72e88e7b127fd8676b0cc1c105e8479a4cdc0847
chaos-yeats <chaos.ye@outlook.com>
2022-05-13 08:39:42 +0800

    验证效果

--- main.go
+++ main.go
@@ -15,6 +15,8 @@ func main() {
 		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 	})
 
+	// curl 模拟表单：https://blog.csdn.net/freedomwjx/article/details/43278157
+	// curl -XPOST "http://127.0.0.1:9999/login" -d "username=hello&password=world"
 	r.Post("/login", func(c *yee.Context) {
 		c.JSON(http.StatusOK, yee.H{
 			"username": c.PostForm("username"),
